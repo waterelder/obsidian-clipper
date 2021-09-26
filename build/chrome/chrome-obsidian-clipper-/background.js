263d266920a7beeb23593a9439bd39acd19f8a55
Lex Kalina <kalinalex91@gmail.com>
2021-09-26 15:39:04 +0300

    Releasing

--- build/chrome/chrome-obsidian-clipper-/background.js
+++ build/chrome/chrome-obsidian-clipper-/background.js
@@ -4,6 +4,42 @@
 // Then this script creates a new tab with a redirect that opens the
 // Obsidian vault with the specified note.
 
+function getword(info,tab) {
+    console.log("Word " + info.selectionText + " was clicked.");
+    chrome.tabs.create({  
+      url: "http://www.google.com/search?q=" + info.selectionText
+    });
+  }
+chrome.contextMenus.create({
+    title: "Save to Obsidian", 
+    contexts:["selection"], 
+    onclick: listener
+});
+
+function listener(result) {
+    // Remove listener to prevent trigger multiple times
+    chrome.runtime.onMessage.removeListener(listener);
+
+    console.log(result)
+    var noteName = result[0]
+    var note = encodeURIComponent(result[1])
+
+    console.log(noteName, note)
+    // Redirect to page (which opens obsidian).
+    if (clipAsNewNote) {
+        redirectUrl = `https://obsidian.kalyna.tech/clip-to-new.html?vault=${vault}&note=${noteName}&content=${encodeURIComponent(note)}`
+        console.log(redirectUrl)
+    } else {
+        redirectUrl = `https://obsidian.kalyna.tech/clip.html?vault=${vault}&note=${noteName}`
+    }
+
+    // Create and remove the extra tab:
+    chrome.tabs.create({ url: redirectUrl , active: true},function(obsidianTab){
+        // Close the tab after one second..
+        setTimeout(function() { chrome.tabs.remove(obsidianTab.id) }, 1000);
+    });
+};
+
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(null, { file: "lib/webbrowser-polyfill.js" }, function() { // https://unpkg.com/webextension-polyfill@0.6.0/dist/browser-polyfill.js
     chrome.tabs.executeScript(null, { file: "lib/jquery.js" }, function() { // https://code.jquery.com/jquery-3.5.1.min.js
@@ -30,29 +66,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         const vault = clippingOptions.obsidianVaultName;
         const clipAsNewNote = clippingOptions.clipAsNewNote;
 
-        chrome.runtime.onMessage.addListener(function listener(result) {
-            // Remove listener to prevent trigger multiple times
-            chrome.runtime.onMessage.removeListener(listener);
-
-            console.log(result)
-            var noteName = result[0]
-            var note = encodeURIComponent(result[1])
-
-            console.log(noteName, note)
-            // Redirect to page (which opens obsidian).
-            if (clipAsNewNote) {
-                redirectUrl = `https://obsidian.kalyna.tech/clip-to-new.html?vault=${vault}&note=${noteName}&content=${encodeURIComponent(note)}`
-                console.log(redirectUrl)
-            } else {
-                redirectUrl = `https://obsidian.kalyna.tech/clip.html?vault=${vault}&note=${noteName}`
-            }
-
-            // Create and remove the extra tab:
-            chrome.tabs.create({ url: redirectUrl , active: true},function(obsidianTab){
-                // Close the tab after one second..
-                setTimeout(function() { chrome.tabs.remove(obsidianTab.id) }, 1000);
-            });
-        });
+        chrome.runtime.onMessage.addListener(listener)
 
         chrome.tabs.executeScript(tab.id, {file: 'clip.js'}, () => {
             console.log("started clipping..")
